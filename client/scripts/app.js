d8fdb0f1a701bb15dac9c4b0089b8fad327d82c9
henryw4k <henryw4k@gmail.com>
2014-12-30 10:52:02 -0800

    Complete Requirement 4.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -21,9 +21,6 @@ _.escape = function(string) {
   });
 };
 
-
-
-
 app.init= function() {
   app.server = 'https://api.parse.com/1/classes/chatterbox';
 };
@@ -39,8 +36,8 @@ app.fetch= function() {
         order: "-createdAt"
       },
       success: function (data) {
-        console.log('chatterbox: got the message');
-        console.log(data);
+        // console.log('chatterbox: got the message');
+        // console.log(data);
 
         for (var i = 0; i < data.results.length; i++) {
           var $username = '<span class = user>'+_.escape(data.results[i].username)+'</span>';
@@ -69,24 +66,6 @@ app.refresh = function() {
 
 
 };
-// var message = {
-//       // createdAt: "2013-10-07T17:24:40.668Z",
-//       // objectId: "8noEr4Pu8J",
-//       username: 'jillian',
-//       text: 'hello',
-//       roomname: 'lobby'
-//       // updatedAt: "2013-10-07T17:24:40.668Z",
-//     };//message from input form
-  // var message = {
-  //   username: $('userBox').val(),
-  //   text: $('textBox').val(),
-  //   roomname: $('roomBox').val()
-  // };
-    //var message = {
-    //   username: $(".userBox").val(),
-    //   texts: $(".messageBox").val(),
-    //   roomname: $(".roomBox").val()
-    // };
 
 
 // LOOK AT CONFIG.JS!!!
@@ -99,36 +78,35 @@ app.send = function(message) {
         data: JSON.stringify(message),
         contentType: 'application/json',
         success: function (data) {
-          // var $username = '<span class = users>'+_.escape(data.username)+'</span>';
-          // var $message = '<span class = messages>'+_.escape(data.texts)+'</span>';
-          // var $time = '<span class=times>' + _.escape(data.roomname) + '</span>'
-          // // $('.chats').append(data.results[i].username + ": ").append(data.results[i].text).append("<br>");
-          //   $('.chats').append($username + ': ' + $message + $time + '<br>');
-          // console.log('chatterbox: Message sent');
-          console.log(data);
+
         },
         error: function (data) {
-          // console.log($('.textBox').val());
+
           // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
           console.error('chatterbox: Failed to send message');
         }
       });
   };
 
-$('.send').on('click', function () {
-   var username = $('.userBox').val();
-   var texts = $('.messageBox').val();
-   var roomName = $('.roomBox').val();
+$(document).ready(function(){
+  $('button.send').on('click', function () {
+     var j_username = $('.userBox').val();
+     var j_texts = $('.messageBox').val();
+     var j_roomName = $('.roomBox').val();
 
-   var message = {
-    'username': username,
-    'text': texts,
-    'roomname': roomName
-   };
+     console.log("send button works");
 
-   console.log(message);
-   app.send(message);
+     var message = {
+      username: j_username,
+      text: j_texts,
+      roomname: j_roomName
+     };
+     app.send(message);
+  });
+
 });
+
 
 app.fetch();
 app.refresh();
+
